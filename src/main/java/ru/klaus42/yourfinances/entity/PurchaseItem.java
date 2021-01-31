bff6b2cf21361fe69f073a23f3dac93f27ec690e
Klaus <neel-cola@mail.ru>
2021-01-31 10:02:44 +0700

    Добавлены сообщения, отображаемые приошибках валидации Добавлено поле общей суммы элемента покупки и метод для ее получения

--- src/main/java/ru/klaus42/yourfinances/entity/PurchaseItem.java
+++ src/main/java/ru/klaus42/yourfinances/entity/PurchaseItem.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Getter
@@ -23,16 +24,24 @@ public class PurchaseItem {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Укажите название")
     private String name = "Новая часть покупки";
 
-    private Float price;
+    @NotNull(message = "Укажите цену")
+    private Double price;
 
+    @Digits(integer=10, fraction=0, message = "Количество должно быть целым числом")
+    private Short quantity = 1;
 
-    private Short quantity;
-
+    @Transient
+    private Double summaryPrice;
 
     @ManyToOne
     @JoinColumn(name = "purchase_id", nullable = false)
     @JsonBackReference
     Purchase purchase;
+
+    public Double getSummaryPrice() {
+        return this.price * this.quantity;
+    }
 }
